70421e66984f013a4043d5662fb7404e99d9557b
gerardohb99 <gerardohb99@gmail.com>
2022-03-30 03:20:00 +0200

    count

---
+++ executeCount.sh
@@ -0,0 +1,51 @@
+
+rm output.arff
+python busters.py -l freeHunt.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l freeHunt5.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-sameMaps-freeHunt.txt
+
+rm output.arff
+python busters.py -l sandClockHunt.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l sandClockHunt5.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-sameMaps-sandClockHunt.txt
+
+rm output.arff
+python busters.py -l theLMap.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l theLMap5.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-sameMaps-theLMap.txt
+
+rm output.arff
+python busters.py -l dotHunt.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l dotHunt5.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-sameMaps-dotHunt.txt
+
+rm output.arff
+python busters.py -l maziMaze.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l maziMaze6.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-sameMaps-maziMaze.txt
+
+# OtherMaps
+rm output.arff
+python busters.py -l testTube.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l testTube.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-otherMaps-testTube.txt
+
+rm output.arff
+python busters.py -l 20Hunt.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l 20Hunt.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-otherMaps-20Hunt.txt
+
+rm output.arff
+python busters.py -l newmap.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l newmap.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-otherMaps-newmap.txt
+
+rm output.arff
+python busters.py -l extra.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l extra.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-otherMaps-extra.txt
+
+rm output.arff
+python busters.py -l capsuleClassic.lay -p BasicAgentAA  -t 0 -g RandomGhost
+python busters.py -l capsuleClassic.lay -p BasicAgentAA -t 0 -g RandomGhost
+wc -l output.txt > salida-otherMaps-capsuleClassic.txt
